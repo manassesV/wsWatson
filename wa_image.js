c8fb39e31ef59042ede53bb0e94d7a66b9385b64
Manassés <manavitorino@gmail.com>
2019-08-24 17:47:25 -0300

    Update wa_image.js

--- wa_image.js
+++ wa_image.js
@@ -2,9 +2,7 @@ var VisualRegognitionV3 = require('ibm-watson/visual-recognition/v3');
 var fs = require('fs');
 
 var visualrecogniton = new VisualRegognitionV3({
-    url: 'https://gateway.watsonplatform.net/visual-recognition/api',
-    version: '2018-03-19',
-    iam_apikey: '5dsxq78CNjD3HOHu9BU0VWBPNSJs-HuZCh7lvNTCgLP1',
+
     disable_ssl_verification: true
 });
 
