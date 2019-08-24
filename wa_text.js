3916f1f0ebf12e8aadb217bcaf3e2156228c6154
Manassés <manavitorino@gmail.com>
2019-08-24 17:48:02 -0300

    Update wa_text.js

--- wa_text.js
+++ wa_text.js
@@ -3,8 +3,7 @@ var TextToSpeechV1 = require('ibm-watson/text-to-speech/v1');
 var fs = require('fs');
 
 var textToSpeech = new TextToSpeechV1({
-    url: 'https://stream.watsonplatform.net/text-to-speech/api',
-    iam_apikey: 'H6ciGbcMflGzyKrLuNEAbqwlgwByDP2l6t4rBZ5q29LD',
+ 
     disable_ssl_verification: true
 });
 
